2a307b8b7db2d6f1393b39c556aea7f498a4903e
Diep Thuy <boychaos9x@gmail.com>
2020-04-26 10:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function shout(string){
-  return string.toUpperCase();  
+function shout(str){
+  return str.toUpperCase();  
+}
+function whisper(str){
+  return str.lowercase();
+}
+function logShout(str){
+  console.log(str.toUpperCase());
+}
+function logWhisper(str){
+  console.log(str.toLowerCase());
+}
+function sayHiToGrandma(str){
+  if (str.toEqual(str.lowercase()))
+   return "I can't hear you!";
 }
