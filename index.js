d3bb353fedd4a79f882a2a99ece3427101982c89
Diep Thuy <boychaos9x@gmail.com>
2020-04-26 10:34:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(str){
+  return str.toUpperCase();
+}
+function whisper(str){
+  return str.toLowerCase();
+}
+function logShout(str){
+  console.log(str.toUpperCase());
+}
+function logWhisper(str){
+  console.log(str.toLowerCase());
+}
+function sayHiToGrandma(string){
+  if (string== string.toLowerCase())
+     return "I can't hear you!";
+
+  if (string== string.toUpperCase())
+    return "YES INDEED!";
+
+  if (string == "I love you, Grandma.")
+      return "I love you, too.";
+
+
+}
